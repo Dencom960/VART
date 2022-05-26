5c65a3488ce393da5f3743df84d159a3823f7961
LikhenkoVG <106115719+LikhenkoVG@users.noreply.github.com>
2022-05-26 22:30:18 +0300

    Версия 0.1
    -Добавил переходы между комнатами
    -Добавил освещение
    -Добавил начальную ДБ

---
+++ Unity_Project_VART/VART/Assets/Sripts/room_manager_scence_key.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class room_manager_scence_key : MonoBehaviour
+{
+    IEnumerator Start()
+    {
+        db_room.room = SceneManager.GetActiveScene().buildIndex; // сохранение комнаты  в db_room
+        yield return new WaitForSeconds(db_room.time); // задержка в комнате, можно менять в db__room
+        SceneManager.LoadScene(0); // возврат на аукцион
+    }
+}
